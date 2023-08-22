edffe43953450f5a53de879fbdb4ddf1ea7bcd84
Yamin Raad <ashabulraad@gmail.com>
2023-08-23 03:54:33 +0600

    update

--- src/app/modules/admin/admin.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createAdmin, getAdmins } from "./admin.controller.js";
-
-const router = Router();
-
-router.post("/createAdmin", createAdmin);
-router.get("/admins", getAdmins);
-
-export default router;
